60139396872da21d3505e25b13f737a2eedd814d
doggopawer <doggopawer@gmail.com>
2022-11-03 14:31:54 +0900

    feat : express, mysql2 세팅 완료

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mysql  from 'mysql2';
+import { mysqlConfig } from './environment.js';
+const app = express()
+
+// create the connection to database
+const connection = mysql.createConnection(mysqlConfig);
+
+connection.connect();
+
+connection.query('SELECT * FROM member', function (error, results, fields) {
+        if(error) {
+            console.log(error);
+        }
+        console.log(results);
+    }
+);
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+
+
+app.listen(4000);
+
